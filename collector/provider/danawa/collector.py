dbffa034b935148dcefd93124d549d8bfbe3f61a
solo5star <solo_5@naver.com>
2021-10-05 21:11:28 +0900

    Implement Provider: danawa

---
+++ collector/provider/danawa/collector.py
@@ -0,0 +1,146 @@
+from typing import Optional
+import re
+from collector.classification import Classification
+from collector.product import Product, ProductSpec
+from collector.tag import resolve_tag
+from ..base.collector import Collector
+from .session import DanawaSession
+
+
+class DanawaCollector(Collector):
+    session: DanawaSession
+
+    def __init__(self, session: DanawaSession):
+        super().__init__(session)
+
+
+class DanawaCPUCollector(DanawaCollector):
+    def do_collect(self, session: DanawaSession, page: int) -> Optional[list[ProductSpec]]:
+        product_specs = []
+        session.load_from_query('CPU', page)
+
+        for raw_product_spec in session.get_product_specs():
+            if raw_product_spec.category[-2:] == ['CPU', 'AMD']:
+                brand = 'AMD'
+            elif raw_product_spec.category[-2:] == ['CPU', '인텔']:
+                brand = '인텔'
+            else:
+                print(f'Note: Skip "{raw_product_spec.name}" is not a CPU')
+                continue
+
+            product_spec = ProductSpec(raw_product_spec)
+
+            # find pattern (*****) in '인텔 코어i5-10세대 10400 (코멧레이크S) (정품)'
+            if (match := re.search(r'\((.+?)\)', product_spec.name)) is not None:
+                match_groups = match.groups()
+                if len(match_groups) > 0:
+                    series = match_groups[0]
+
+            product_spec.classification = Classification(
+                category='CPU',
+                level1=brand,
+                level2=series,
+            )
+            product_specs.append(product_spec)
+
+        return product_specs
+
+
+class DanawaVGACollector(DanawaCollector):
+    def do_collect(self, session: DanawaSession, page: int) -> Optional[list[ProductSpec]]:
+        product_specs = []
+
+        session.load_from_query('NVIDIA 그래픽카드', page)
+        tags_dictionary = session.get_search_tags_dictionary()
+
+        for raw_product_spec in session.get_product_specs():
+            if raw_product_spec.category[-1:] != ['그래픽카드(VGA)']:
+                print(f'Note: Skip "{raw_product_spec.name}" is not a 그래픽카드')
+                continue
+
+            product_spec = ProductSpec(raw_product_spec)
+            series_if_nvidia = resolve_tag(product_spec.tags, tags_dictionary.get('NVIDIA 칩셋', []))
+            series_if_amd = resolve_tag(product_spec.tags, tags_dictionary.get('AMD 칩셋', []))
+
+            if series_if_nvidia is not None:
+                brand = 'NVIDIA'
+                series = series_if_nvidia
+            elif series_if_amd is not None:
+                brand = 'AMD'
+                series = series_if_amd
+            else:
+                print(f'Note: Skip "{product_spec.name}" cannot resolve series')
+                continue
+
+            product_spec.classification = Classification(
+                category='그래픽카드',
+                level1=brand,
+                level2=series,
+            )
+            product_specs.append(product_spec)
+
+        return product_specs
+
+
+class DanawaMainboardCollector(DanawaCollector):
+    def do_collect(self, session: DanawaSession, page: int) -> Optional[list[ProductSpec]]:
+        product_specs = []
+
+        session.load_from_query('메인보드', page)
+        tags_dictionary = session.get_search_tags_dictionary()
+
+        for raw_product_spec in session.get_product_specs():
+            if raw_product_spec.category[-1:] != ['메인보드']:
+                print(f'Note: Skip "{raw_product_spec.name}" is not a 메인보드')
+                continue
+
+            product_spec = ProductSpec(raw_product_spec)
+            chipset = resolve_tag(product_spec.tags, tags_dictionary.get('세부 칩셋', []))
+            
+            if chipset is None:
+                print(f'Note: Skip "{product_spec.name}" cannot resolve chipset')
+                continue
+
+            chipset_match = re.search(r'\((.+?)\)', chipset)
+            if chipset_match is None or (brand := chipset_match.groups()[0]) is None:
+                print(f'Note: Skip "{product_spec.name}" cannot resolve brand')
+                continue
+            
+            product_spec.classification = Classification(
+                category='메인보드',
+                level1=brand,
+                level2=chipset,
+            )
+            product_specs.append(product_spec)
+        
+        return product_specs
+
+
+class DanawaRAMCollector(DanawaCollector):
+    def do_collect(self, session: DanawaSession, page: int) -> Optional[list[ProductSpec]]:
+        product_specs = []
+
+        session.load_from_query('RAM', page)
+        tags_dictionary = session.get_search_tags_dictionary()
+
+        for raw_product_spec in session.get_product_specs():
+            if raw_product_spec.category[-1:] != ['RAM']:
+                print(f'Note: Skip "{raw_product_spec.name}" is not a RAM')
+                continue
+
+            product_spec = ProductSpec(raw_product_spec)
+            target_device = resolve_tag(product_spec.tags, tags_dictionary.get('사용 장치', []))
+            series = resolve_tag(product_spec.tags, tags_dictionary.get('제품 분류', []))
+            clock_speed = resolve_tag(product_spec.tags, tags_dictionary.get('동작클럭(대역폭)', []))
+
+            if clock_speed is not None:
+                series = f'{series} {clock_speed}'
+
+            product_spec.classification = Classification(
+                category='RAM',
+                level1=target_device,
+                level2=series,
+            )
+            product_specs.append(product_spec)
+        
+        return product_specs
---
+++ collector/provider/danawa/session.py
@@ -0,0 +1,104 @@
+from typing import Dict
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+import re
+from collector.tag import TagsDictionary
+from ..base.product import RawProduct, RawProductSpec
+from ..base.session import Session
+
+
+class DanawaParseError(Exception):
+    pass
+
+
+class DanawaSession(Session):
+    name = 'danawa'
+
+    def __parse_product(self, element: Tag) -> RawProduct:
+        product = RawProduct()
+        product.id = re.search(r'productInfoDetail_(\d+)', element.attrs['id']).groups()[0]
+
+        if (variant := element.select_one('.memory_sect')) is not None:
+            product.variant = ' '.join(filter(lambda token: len(token) > 0 and not re.match(r'\d+위', token), map(lambda token: token.strip(), variant.find_all(text=True))))
+        else:
+            product.variant = None
+
+        price_text = element.select_one('.price_sect > a > strong').text.strip().replace(',', '')
+        if price_text.isnumeric():
+            product.price = int(price_text)
+            product.stock_state = '재고있음'
+        else:
+            product.price = None
+            product.stock_state = price_text
+
+        return product
+
+
+    def __parse_product_spec(self, element: Tag) -> RawProductSpec:
+        product_spec = RawProductSpec()
+
+        product_spec.id = re.search(r'\?pcode=(.+?)\&', element.select_one('.prod_name > a').attrs['href']).groups()[0]
+        product_spec.name = element.select_one('.prod_name > a').text
+
+        thumbnail = element.select_one('.thumb_image img')
+        if 'src' in thumbnail.attrs:
+            product_spec.thumbnail = thumbnail.attrs['src']
+        else:
+            product_spec.thumbnail = thumbnail.attrs['data-original']
+
+        product_spec.tags = [*map(lambda spec: spec.strip(), element.select_one('.prod_spec_set .spec_list').text.strip().split(' / '))]
+
+        if (registration_date := element.select_one('.prod_sub_meta > .mt_date > dd')) is None:
+            return None
+
+        # example: 2020.11
+        registration_year, registration_month = registration_date.text.split('.')
+        product_spec.registration_date = f'{registration_year}-{registration_month}'
+
+        # example: PC주요부품 > CPU > AMD (카테고리가 없으면 SKIP)
+        if (category := element.select_one('.prod_category_location > dd')) is None:
+            raise DanawaParseError(f'Note: Skip product spec "{product_spec.name}", because it does not have category location.')
+
+        product_spec.category = [*map(lambda category: category.strip(), category.text.strip().split('>'))]
+        product_spec.products = [*map(self.__parse_product, element.select('.prod_pricelist li'))]
+
+        return product_spec
+
+
+    def load_from_query(self, query: str, page: int = 1):
+        self.latest_response = self.driver.get(f'http://search.danawa.com/dsearch.php?query={query}&page={page}&limit=120')
+
+
+    def get_product_specs(self) -> list[RawProductSpec]:
+        self.assert_page_loaded()
+
+        soup = BeautifulSoup(self.latest_response.content, 'html.parser')
+        products = []
+
+        for product_element in soup.select('#productListArea .product_list > .prod_item'):
+            # 파싱에 필요한 속성(id)가 부족함
+            if not ('id' in product_element.attrs and product_element.attrs['id'].startswith('productItem')):
+                continue
+            
+            try:
+                product = self.__parse_product_spec(product_element)
+            except DanawaParseError as error:
+                print(error)
+
+            products.append(product)
+
+        return products
+
+
+    def get_search_tags_dictionary(self) -> TagsDictionary:
+        self.assert_page_loaded()
+
+        soup = BeautifulSoup(self.latest_response.content, 'html.parser')
+        groups = {}
+        for group_area in soup.select('.basic_cate_area'):
+            group_name = group_area.select_one('.cate_head').text.strip()
+
+            keywords = group_area.select_one('.basic_sub_area') or group_area.select_one('.basic_top_area')
+            groups[group_name] = [*map(lambda element: element.text.strip(), keywords.select('.basic_cate_item'))]
+
+        return groups
