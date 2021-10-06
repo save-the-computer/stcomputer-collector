eead8d68985944eb14d895a8211d6a86fa774b32
solo5star <solo_5@naver.com>
2021-10-06 19:22:37 +0900

    Add setup files for pip installation

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stcomputer-collector',
+    version='0.1.0',
+    description='Collector that collect product data from external providers such as danawa',
+    author='solo5star',
+    author_email='solo_5@naver.com',
+    url='https://github.com/save-the-computer/collector',
+    download_url='https://github.com/save-the-computer/collector/archive/0.1.0.tar.gz',
+    install_requires=[],
+    packages=find_packages(exclude=[]),
+    keywords=[],
+    python_requires='>=3',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+)
