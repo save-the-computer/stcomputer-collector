4bbf16e57f65c75eadfdec392e6e45cc9211501f
solo5star <solo_5@naver.com>
2021-10-06 22:14:04 +0900

    move some metadata to stcomputer_collector/__init__.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup, find_packages
+import stcomputer_collector
 
 with open('requirements.txt') as f:
     requires = f.read().splitlines()
 
 setup(
     name='stcomputer-collector',
-    version='0.1.0',
+    version=stcomputer_collector.__version__,
     description='Collector that collect product data from external providers such as danawa',
-    author='solo5star',
+    author=stcomputer_collector.__author__,
     author_email='solo_5@naver.com',
     url='https://github.com/save-the-computer/collector',
     download_url='https://github.com/save-the-computer/collector/archive/0.1.0.tar.gz',
