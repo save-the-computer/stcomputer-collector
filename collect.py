a6548b550a5ceed64d614f169738b37a94b6835d
solo5star <solo_5@naver.com>
2021-11-09 20:22:08 +0900

    collect.py: use argparse module for command line argument parsing. and add page_limit option

--- collect.py
+++ collect.py
@@ -1,41 +1,30 @@
 import sys
+import argparse
 import jsonpickle
 from stcomputer_collector.collectors import collectors
 
 
-def help():
-    print(
-        '쇼핑몰 서비스(danawa 등)로부터 제품 목록 데이터를 크롤링합니다.\n'
-        'Collector를 지정하면 해당 Collector를 사용하여 제품 목록을 가져옵니다.\n'
-        '제품 목록 데이터는 json 포맷으로 outputs 폴더에 저장됩니다.\n'
-        '\n'
-        '사용법\n'
-        '\tcollect.py <collector>\n'
-        '\n'
-        '사용 가능한 collectors 목록'
-    )
-    for collector_name in collectors.keys():
-        print(f'\t{collector_name}')
-        print(f'\t\t{collector_name} 카테고리의 제품 목록을 크롤링합니다.')
-        print()
+parser = argparse.ArgumentParser(
+    description=
+        '''쇼핑몰 서비스(danawa 등)로부터 제품 목록 데이터를 크롤링합니다.
+Collector를 지정하면 해당 Collector를 사용하여 제품 목록을 가져옵니다.
+제품 목록 데이터는 json 포맷으로 outputs 폴더에 저장됩니다.''',
+    formatter_class=argparse.RawTextHelpFormatter
+)
+
+parser.add_argument('collector', choices=collectors, help=f'사용할 Collector입니다.')
+parser.add_argument('--page-limit', type=int, default=1, help='크롤링할 페이지 수를 지정합니다. 기본값은 1입니다.')
 
 
 def main():
-    params = sys.argv[1:]
-    if len(params) == 0:
-        help()
-        return
+    args = parser.parse_args()
     
-    collector_name = params[0]
-    if collector_name not in collectors:
-        print(f'"{collector_name}" 는 존재하지 않는 Collector입니다.')
-        return
-    
+    collector_name = args.collector
     collector = collectors[collector_name]
     print(f'"{collector_name}" Collector를 사용하여 크롤링을 시작합니다 ...')
     
     product_specs = []
-    for batch_product_specs in collector.collect(1):
+    for batch_product_specs in collector.collect(args.page_limit):
         product_specs += batch_product_specs
     
     # jsonpickle 모듈 환경설정
